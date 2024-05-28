2eb764c2f82834bbd874e021c83393907710f84b
Francisco Javier Florencio Airado <fjflorenc@gmail.com>
2024-05-28 21:50:43 +0200

    introducing signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-user';
 
@@ -12,15 +12,29 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
 
-  get imagePath(){ // as it has the key word GET, we use it in the html like a property.
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  /*
+    SIGNALS: notify angular when a value changes 
+    or the UI needs to be updated.
+  */
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  /*
+    this is the manner to use a computed value with signals
+    Angular sets up a subscription to the signal and updates
+    the value whenever the signal changes, and only then angular
+    recompute the imgPath value.
+  */
+  imagePath = computed( () => 'assets/users/' + this.selectedUser().avatar);
+
+  // get imagePath(){ // as it has the key word GET, we use it in the html like a property.
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+
   }
 
 }
