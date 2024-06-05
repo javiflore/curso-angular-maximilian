c40ce63d0da30d19ceb8f5c8cc3d11ce56e14a2f
Francisco Javier Florencio Airado <fjflorenc@gmail.com>
2024-06-05 23:20:45 +0200

    new component tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-user';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,8 +16,14 @@ export class AppComponent {
 
   users = DUMMY_USERS;
 
+  nameSelected = '';
+
   onUserSelected(id: string) {
-    console.log('User selected:', id);
+    this.users.find(user => {
+      if (user.id === id) {
+        this.nameSelected = user.name;
+      }
+    })
   }
 
 }
