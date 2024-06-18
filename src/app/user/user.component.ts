bc17e7f46487c9eedc8216c45d2a22d68d816042
Francisco Javier Florencio Airado <fjflorenc@gmail.com>
2024-06-18 21:03:26 +0200

    object type

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,18 +9,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    name: string;
+    avatar: string;
+  };
+  
   @Output() userSelected = new EventEmitter<string>();     
 
   get imagePath(){
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
 
   onSelectUser() {
-    this.userSelected.emit(this.id);
+    this.userSelected.emit(this.user.id);
   }
 
 }
