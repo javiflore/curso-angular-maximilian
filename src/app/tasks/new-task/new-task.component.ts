be939fd5800c9b4a01a88751189398902c4cd609
Francisco Javier Florencio Airado <fjflorenc@gmail.com>
2024-06-23 13:09:19 +0200

    signals and two-way-binding

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,15 +1,21 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
 
   @Output() closeModal = new EventEmitter<void>();
+
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
+
 
   onCloseModal(){
     this.closeModal.emit();
